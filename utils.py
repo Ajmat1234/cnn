ea3ffcd3ed67e87454b0187809a9b3f108e98f0f
Ajmat <hunterboyz73770@gmail.com>
2025-12-14 17:32:03 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,36 +1,28 @@
 import torch
 
 class strLabelConverter(object):
-    def __init__(self, alphabet, ignore_case=False):
-        self.alphabet = alphabet + '-'  # blank at the end
-        self.dict = {char: i + 1 for i, char in enumerate(alphabet)}
-        self.ignore_case = ignore_case
+    def __init__(self, alphabet):
+        self.alphabet = alphabet
+        self.dict = {c: i + 1 for i, c in enumerate(alphabet)}  # blank=0 implicit
 
-    def encode(self, text):
-        length = []
+    def encode(self, texts):
+        lengths = [len(text) for text in texts]
         result = []
-        for item in text:
-            item = item if not self.ignore_case else item.lower()
-            length.append(len(item))
-            for char in item:
-                if char not in self.dict:
-                    continue  # skip unknown chars
-                result.append(self.dict[char])
-        text = torch.IntTensor(result)
-        length = torch.IntTensor(length)
-        return text, length
+        for text in texts:
+            for char in text:
+                if char in self.dict:
+                    result.append(self.dict[char])
+        return torch.IntTensor(result), torch.IntTensor(lengths)
 
-    def decode(self, t, length, raw=False):
-        if raw:
-            return ''.join([self.alphabet[i - 1] for i in t if i > 0])
+    def decode(self, preds, lengths):
         texts = []
         index = 0
-        for l in length:
-            txt = []
+        for l in lengths:
+            t = preds[index:index + l]
+            char_list = []
             for i in range(l):
-                char_idx = t[index + i]
-                if char_idx > 0 and (i == 0 or t[index + i - 1] != char_idx):
-                    txt.append(self.alphabet[char_idx - 1])
-            texts.append(''.join(txt))
+                if t[i] != 0 and (i == 0 or t[i] != t[i - 1]):
+                    char_list.append(self.alphabet[t[i] - 1])
+            texts.append(''.join(char_list))
             index += l
         return texts
